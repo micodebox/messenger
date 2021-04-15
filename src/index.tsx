c60b4e7ba3ab975af28b032c0d5d53986dbb9cde
micodebox <mcmicdesong@gmail.com>
2021-04-15 11:29:30 -0400

    Init app environment for eslint and typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { AppProviders } from 'providers/AppProviders';
+import { App } from 'app/App';
+
+import * as serviceWorker from './serviceWorker';
+
+const app = (
+  <AppProviders>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </AppProviders>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = document.getElementById('root');
+
+ReactDOM.render(app, root);
+
+serviceWorker.unregister();
---
+++ src/providers/AppProviders.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { AppProvidersProps } from './AppProviders.types';
+
+export const AppProviders = ({ children }: AppProvidersProps) => <>{children}</>;
